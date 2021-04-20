5a0f1870d344039c31f4331547e3ea74eceaa74b
Alex Whitt <alex.joseph.whitt@gmail.com>
2021-04-19 22:56:19 -0400

    BLTouch offset with bullseye hotend duct

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -952,7 +952,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -14, -0.6 }
+#define NOZZLE_TO_PROBE_OFFSET { -48, -10, -1.7 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
