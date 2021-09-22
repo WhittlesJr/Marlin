2d6cd99128511440351e1882001fd0d38248d14f
Alex Whitt <alex.joseph.whitt@gmail.com>
2021-09-22 14:44:10 -0400

    Disable linear advance for now

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1500,10 +1500,16 @@
  * See http://marlinfw.org/docs/features/lin_advance.html for full instructions.
  * Mention @Sebastianv650 on GitHub to alert the author of any issues.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
+
+  // eSUN PLA+
   #define LIN_ADVANCE_K 0.29    // Unit: mm compression per 1mm/s extruder speed
+
+  //Generic PLA
+  //#define LIN_ADVANCE_K 0.29    // Unit: mm compression per 1mm/s extruder speed
+
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
 #endif
 
