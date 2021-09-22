3b72ecdf2ab54e65fdbaa905ec266f7e5113ecf4
Alex Whitt <alex.joseph.whitt@gmail.com>
2021-09-22 14:44:24 -0400

    Enable probe repeatability

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1005,7 +1005,7 @@
 #define Z_PROBE_OFFSET_RANGE_MAX 20
 
 // Enable the M48 repeatability test to test probe accuracy
-//#define Z_MIN_PROBE_REPEATABILITY_TEST
+#define Z_MIN_PROBE_REPEATABILITY_TEST
 
 // Before deploy/stow pause for user confirmation
 //#define PAUSE_BEFORE_DEPLOY_STOW
