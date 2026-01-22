174cf1540e1548a248066b1b76d480ef8e488710
Alex Whitt <alex.joseph.whitt@gmail.com>
2026-01-22 10:37:54 -0500

    Tune z nozzle offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1699,7 +1699,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -48.65, -13.4, -0.6 } // (mm) X, Y, Z distance from Nozzle tip to Probe trigger-point
+#define NOZZLE_TO_PROBE_OFFSET { -48.65, -13.4, -3.2 } // (mm) X, Y, Z distance from Nozzle tip to Probe trigger-point
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
